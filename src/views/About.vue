377736a043fb7b4b0fa5cb16cbf515bc429c224f
eno <muhammadrisano@gmail.com>
2020-06-10 15:31:45 +0700

    add

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Data buku</h1>
+    <ul>
+      <li v-for="(book, index) in books" :key="index">
+        {{book.author}}
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'About',
+  data () {
+    return {
+      books: []
+    }
+  },
+  mounted () {
+    axios.get('http://34.204.60.85:8001/api/v1/book')
+      .then((res) => {
+        this.books = res.data.result
+      })
+  }
+}
+</script>
